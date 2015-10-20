183417f4e304fdaeb926cf36424acbb9281cf17e
Aragas <micvitalij@gmail.com>
2015-10-20 16:28:51 +0300

    Using Sockets

--- WrapperInstances/TCPClientWrapperInstance.cs
+++ WrapperInstances/TCPClientWrapperInstance.cs
@@ -1,181 +1,34 @@
-﻿using System.Diagnostics;
-using System.IO;
-using System.Linq;
+﻿using System.IO;
 using System.Net;
-using System.Net.NetworkInformation;
 using System.Net.Sockets;
 
 using Aragas.Core.Wrappers;
 
 namespace PokeD.Server.Desktop.WrapperInstances
 {
-    public class TCPClientClass1 : ITCPClient
-    {
-        #region Connection Stuff
-        private static int RefreshConnectionInfoTimeStatic { get; set; } = 5000;
-        private static Stopwatch ConnectedTCPRefresh { get; } = Stopwatch.StartNew();
-
-        private static TcpConnectionInformation[] _connectedTCPs;
-        private static TcpConnectionInformation[] ConnectedTCPs
-        {
-            get
-            {
-                if (ConnectedTCPRefresh.ElapsedMilliseconds > RefreshConnectionInfoTimeStatic)
-                    UpdateConnectedTCPs();
-
-                return _connectedTCPs;
-            }
-            set { _connectedTCPs = value; }
-        }
-        private static void UpdateConnectedTCPs()
-        {
-            ConnectedTCPRefresh.Restart();
-            ConnectedTCPs = IPGlobalProperties.GetIPGlobalProperties().GetActiveTcpConnections();
-        }
-        #endregion Connection Stuff
-
-        public int RefreshConnectionInfoTime { get { return RefreshConnectionInfoTimeStatic; } set { RefreshConnectionInfoTimeStatic = value; } }
-
-        public bool Connected
-        {
-            get
-            {
-                if (IsDisposed || Client == null)
-                    return false;
-
-                var tcpConnections = ConnectedTCPs
-                    .Where(x => x.LocalEndPoint.Equals(Client.Client.LocalEndPoint) && x.RemoteEndPoint.Equals(Client.Client.RemoteEndPoint)).ToArray();
-
-                if (tcpConnections.Length > 0)
-                {
-                    var stateOfConnection = tcpConnections.First().State;
-
-                    return stateOfConnection == TcpState.Established;
-                }
-                else
-                    return false;
-            }
-        }
-
-        public string IP => !IsDisposed && Client != null ? ((IPEndPoint) Client.Client.RemoteEndPoint).Address.ToString() : "";
-        public int DataAvailable => !IsDisposed && Client != null ? Client.Available : 0;
-
-
-        private TcpClient Client { get; set; }
-        private Stream Stream { get; set; }
-
-        private bool IsDisposed { get; set; }
-
-
-        public TCPClientClass1() { }
-        internal TCPClientClass1(TcpClient tcpClient)
-        {
-            Client = tcpClient;
-            Client.SendTimeout = 5;
-            Client.ReceiveTimeout = 5;
-            Client.NoDelay = false;
-            Stream = Client.GetStream();
-
-            UpdateConnectedTCPs();
-        }
-
-
-        public ITCPClient Connect(string ip, ushort port)
-        {
-            if (Connected)
-                Disconnect();
-
-            Client = new TcpClient(ip, port) { SendTimeout = 5, ReceiveTimeout = 5, NoDelay = false };
-            Stream = Client.GetStream();
-
-            UpdateConnectedTCPs();
-
-            return this;
-        }
-        public ITCPClient Disconnect()
-        {
-            if (Client.Connected)
-                Client.Client.Disconnect(false);
-
-            return this;
-        }
-
-        public void Send(byte[] bytes, int offset, int count)
-        {
-            if (IsDisposed)
-                return;
-
-            try { Stream.Write(bytes, offset, count); }
-            catch (IOException) { Dispose(); }
-            catch (SocketException) { Dispose(); }
-        }
-        public int Receive(byte[] buffer, int offset, int count)
-        {
-            if (IsDisposed)
-                return -1;
-
-            try { return Stream.Read(buffer, offset, count); }
-            catch (IOException) { Dispose(); return -1; }
-            catch (SocketException) { Dispose(); return -1; }
-        }
-
-        public void WriteByteArray(byte[] array)
-        {
-            
-        }
-
-        public byte[] ReadByteArray(int length)
-        {
-            return null;
-        }
-
-        public Stream GetStream()
-        {
-            return Stream;
-        }
-
-        public ITCPClientWrapper NewInstance()
-        {
-            return new TCPClientWrapperInstance();
-        }
-
-
-        public void Dispose()
-        {
-            if (IsDisposed)
-                return;
-
-            Disconnect();
-
-            IsDisposed = true;
-
-            Client?.Close();
-            Stream?.Dispose();
-        }
-    }
-
     public class TCPClientClass : ITCPClient
     {
         public int RefreshConnectionInfoTime { get; set; }
 
-        public string IP => !IsDisposed && Client != null && Client.Client.Connected ? ((IPEndPoint) Client.Client.RemoteEndPoint).Address.ToString() : "";
-        public bool Connected => !IsDisposed && Client != null && Client.Client.Connected;
+        public string IP => !IsDisposed && Client != null ? (Client.RemoteEndPoint as IPEndPoint)?.Address.ToString() : "";
+        public bool Connected => !IsDisposed && Client != null && Client.Connected;
         public int DataAvailable => !IsDisposed && Client != null ? Client.Available : 0;
 
-        private TcpClient Client { get; set; }
+        private Socket Client { get; }
         private Stream Stream { get; set; }
 
         private bool IsDisposed { get; set; }
 
 
-        public TCPClientClass() { }
-        public TCPClientClass(TcpClient tcpClient)
+        public TCPClientClass()
         {
-            Client = tcpClient;
-            Client.SendTimeout = 5;
-            Client.ReceiveTimeout = 5;
-            Client.NoDelay = false;
-            Stream = Client.GetStream();
+            Client = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            Client.NoDelay = true;
+        }
+        internal TCPClientClass(Socket socket)
+        {
+            Client = socket;
+            Stream = new NetworkStream(Client);
 
         }
         
@@ -184,15 +37,15 @@ namespace PokeD.Server.Desktop.WrapperInstances
             if (Connected)
                 Disconnect();
 
-            Client = new TcpClient(ip, port) { SendTimeout = 5, ReceiveTimeout = 5, NoDelay = false };
-            Stream = Client.GetStream();
+            Client.Connect(ip, port);
+            Stream = new NetworkStream(Client);
 
             return this;
         }
         public ITCPClient Disconnect()
         {
             if (Connected)
-                Client.Client.Disconnect(false);
+                Client.Disconnect(false);
 
             return this;
         }
@@ -205,23 +58,10 @@ namespace PokeD.Server.Desktop.WrapperInstances
             try
             {
                 var length = array.Length;
-                var buffer = length < Client.SendBufferSize ?
-                    new byte[length] : 
-                    new byte[Client.ReceiveBufferSize];
-                
-                var totalWritedLength = 0;
-                using (var data = new MemoryStream(array))
-                {
-                    do
-                    {
-                        var writedLength = data.Read(buffer, 0, buffer.Length);
-                        Stream.Write(buffer, 0, buffer.Length);
-                        totalWritedLength += writedLength;
-                    } while (totalWritedLength < length);
 
-                    if(length >= 46857)
-                        Logger.Log(LogType.GlobalError, $"TCPClientClass: WriteByteArray: length >= 46857; Length - {length}, totalWri - {totalWritedLength}, data.Length - {data.Length}");
-                }
+                var bytesSend = 0;
+                while (bytesSend < length)
+                    bytesSend += Client.Send(array, bytesSend, length - bytesSend, 0);
             }
             catch (IOException) { Dispose(); }
             catch (SocketException) { Dispose(); }
@@ -233,28 +73,13 @@ namespace PokeD.Server.Desktop.WrapperInstances
 
             try
             {
-                var buffer = length < Client.ReceiveBufferSize ?
-                    new byte[length] :
-                    new byte[Client.ReceiveBufferSize];
+                var array = new byte[length];
 
-                var totalNumberOfBytesRead = 0;
-                using (var receivedData = new MemoryStream())
-                {
-                    do
-                    {
-                        var numberOfBytesRead = Stream.Read(buffer, 0, buffer.Length);
-                        if (numberOfBytesRead == 0)
-                        {
-                            Logger.Log(LogType.GlobalError, $"TCPClientClass: ReadByteArray: numberOfBytesRead == 0; Length - {length}, totalRec - {totalNumberOfBytesRead}, stream.Length - {receivedData.Length}");
-                            break;
-                        }
+                var bytesReceive = 0;
+                while (bytesReceive < length)
+                    bytesReceive += Client.Receive(array, bytesReceive, length - bytesReceive, 0);
 
-                        receivedData.Write(buffer, 0, buffer.Length); //Write to memory stream
-                        totalNumberOfBytesRead += numberOfBytesRead;
-                    } while (totalNumberOfBytesRead < length);
-
-                    return receivedData.ToArray();
-                }
+                return array;
             }
             catch (IOException) { Dispose(); return new byte[0]; }
             catch (SocketException) { Dispose(); return new byte[0]; }
@@ -269,7 +94,7 @@ namespace PokeD.Server.Desktop.WrapperInstances
 
             IsDisposed = true;
 
-            Client?.Close();
+            Client?.Dispose();
             Stream?.Dispose();
         }
     }
--- WrapperInstances/TCPServerWrapperInstance.cs
+++ WrapperInstances/TCPServerWrapperInstance.cs
@@ -8,19 +8,56 @@ namespace PokeD.Server.Desktop.WrapperInstances
     public class TCPListenerClass : ITCPListener
     {
         public ushort Port { get; }
-        public bool AvailableClients => Listener.Pending();
+        public bool AvailableClients => Listener.Poll(0, SelectMode.SelectRead);
 
-        private TcpListener Listener { get; }
+        private bool IsDisposed { get; set; }
+
+        private Socket Listener { get; }
 
 
-        internal TCPListenerClass(ushort port) { Port = port; Listener = new TcpListener(new IPEndPoint(IPAddress.Any, Port)); }
+        internal TCPListenerClass(ushort port)
+        {
+            Port = port;
 
-        public void Start() { Listener.Start(); }
-        public void Stop() { Listener.Stop(); }
+            Listener = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            Listener.NoDelay = true;
 
-        public ITCPClient AcceptNetworkTCPClient() { return new TCPClientClass(Listener.AcceptTcpClient()); }
+            Listener.Bind(new IPEndPoint(IPAddress.Any, Port));
+        }
 
-        public void Dispose() { Listener?.Stop(); }
+        public void Start()
+        {
+            if (IsDisposed)
+                return;
+
+            Listener.Listen(1000);
+        }
+
+        public void Stop()
+        {
+            if (IsDisposed)
+                return;
+
+            Listener.Close();
+        }
+
+        public ITCPClient AcceptTCPClient()
+        {
+            if (IsDisposed)
+                return null;
+
+            return new TCPClientClass(Listener.Accept());
+        }
+
+        public void Dispose()
+        {
+            if (IsDisposed)
+                return;
+
+            IsDisposed = true;
+
+            Listener?.Dispose();
+        }
     }
 
     public class ITCPServerWrapperrInstance : ITCPListenerWrapper
