78edb150b2a1931adb2e886351e618b277f8ab36
Aragas <micvitalij@yandex.ru>
2018-04-28 15:22:58 +0300

    Removed old lua scripts

--- PokeD.Server.NetCore/Scripts/OldLua/lang/ru.lua
+++
@@ -1,7 +0,0 @@
-﻿lang = { }
-
-lang.Greetings = "Привет, {name}!"
-
-lang.Tournament1 = "Ты хочешь участвовать в {name}?"
-lang.Tournament2 = "Ты записан на {name}!"
-lang.Tournament3 = "Ты не записан на {name}!"
