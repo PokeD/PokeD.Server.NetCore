46375f36aa1393c85bb2091e0ae284ef3ec7009d
Aragas <micvitalij@yandex.ru>
2018-04-28 15:23:44 +0300

    Removed auto update stuff

--- PokeD.Server.NetCore/Storage/Files/UpdaterFile.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace PokeD.Server.NetCore.Storage.Files
-{
-    internal sealed class UpdaterFile : BaseExeFile
-    {
-        public UpdaterFile() : base("PokeD.Server.Desktop.Updater.exe") { }
-    }
-}
